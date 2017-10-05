0073c273aa2cc3688a6d12934627eca6eb6d1261
Roman Iakobchuk <r.iakobchuk@gmail.com>
2017-10-05 22:37:33 +0300

    add constatns

--- src/reducer/counter.js
+++ src/reducer/counter.js
@@ -1,4 +1,5 @@
+import {INCREMENT} from '../constants'
 
 export default function counterReducer(state = 10, action) {
-    return action.type === 'INCREMENT' ? state + 1 : state
+    return action.type === INCREMENT ? state + 1 : state
 }
