a501493e9b1bc6164a78e672611e9f1a46e481cc
Roman Iakobchuk <kaboom.ua@gmail.com>
2017-10-19 22:07:07 +0300

    add immutable

--- src/reducer/utils.js
+++ src/reducer/utils.js
@@ -1,8 +1,7 @@
-export function arrToMap(arr) {
-    return arr.reduce((acc, item) => ({
-        ...acc,
-        [item.id]: item
-    }), {})
+import {Map} from 'immutable'
+
+export function arrToMap(arr, ItemRecord) {
+    return arr.reduce((acc, item) => acc.set(item.id, ItemRecord ? new ItemRecord(item) : item), new Map({}))
 }
 
 export function mapToArr(obj) {
