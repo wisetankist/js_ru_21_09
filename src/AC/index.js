f83931b4e4192bccc9d28da851be845209e4aa3e
Roman Iakobchuk <kaboom.ua@gmail.com>
2017-10-26 12:11:52 +0300

    add HW7

--- src/AC/index.js
+++ src/AC/index.js
@@ -1,5 +1,5 @@
 import {INCREMENT, DELETE_ARTICLE, CHANGE_DATE_RANGE, CHANGE_SELECTION, ADD_COMMENT, LOAD_ALL_ARTICLES,
-    LOAD_ARTICLE, LOAD_ARTICLE_COMMENTS, START, SUCCESS, FAIL} from '../constants'
+    LOAD_ARTICLE, LOAD_ARTICLE_COMMENTS, LOAD_COMMENTS_FOR_PAGE, START, SUCCESS, FAIL} from '../constants'
 
 export function increment() {
     return {
@@ -79,5 +79,18 @@ export function loadArticleComments(articleId) {
         type: LOAD_ARTICLE_COMMENTS,
         payload: { articleId },
         callAPI: `/api/comment?article=${articleId}`
+    }
+}
+
+export function checkAndLoadCommentsForPage(page) {
+    return (dispatch, getState) => {
+        const {comments: {pagination}} = getState()
+        if (pagination.getIn([page, 'loading']) || pagination.getIn([page, 'ids'])) return
+
+        dispatch({
+            type: LOAD_COMMENTS_FOR_PAGE,
+            payload: { page },
+            callAPI: `/api/comment?limit=5&offset=${(page - 1) * 5}`
+        })
     }
 }
--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {Route, NavLink} from 'react-router-dom'
 import ArticlesPage from './routes/ArticlesPage'
+import CommentsPage from './routes/CommentsPage'
 import Filters from './Filters'
 import Counter from './Counter'
 
@@ -19,11 +20,13 @@ class App extends Component {
                     <li><NavLink to = '/articles' activeStyle = {{color: 'red'}}>articles</NavLink></li>
                     <li><NavLink to = '/filters' activeStyle = {{color: 'red'}}>filters</NavLink></li>
                     <li><NavLink to = '/counter' activeStyle = {{color: 'red'}}>counter</NavLink></li>
+                    <li><NavLink to = '/comments/1' activeStyle = {{color: 'red'}}>comments</NavLink></li>
                 </ul>
                 User: <input type = 'text' value = {username} onChange = {this.handleUserChange}/>
                 <Route path = '/counter' component = {Counter} />
                 <Route path = '/filters' component = {Filters} />
                 <Route path = '/articles' component = {ArticlesPage} />
+                <Route path = '/comments' component = {CommentsPage}/>
             </div>
         )
     }
